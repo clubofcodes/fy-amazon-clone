e10b00f37992cfaf2aa3b7d5cb4c34f31df4069e
Rahul Jagetiya <63360644+clubofcodes@users.noreply.github.com>
2022-03-17 23:45:17 +0530

    Card view of highliting products in home.

--- src/Layouts/Home/Banner.jsx
+++ src/Layouts/Home/Banner.jsx
@@ -3,16 +3,30 @@ import './Banner.css';
 
 export const Banner = () => {
     const bannerImgList = [
-        "https://m.media-amazon.com/images/I/81qFlA2dqjL._SX3000_.jpg",
-        "https://m.media-amazon.com/images/I/61FuWeCuGCL._SX3000_.jpg",
-        "https://m.media-amazon.com/images/I/91Rd9TYGhNL._SX3000_.jpg",
         "https://m.media-amazon.com/images/I/71YyQch1rQL._SX3000_.jpg",
+        "https://m.media-amazon.com/images/I/61FuWeCuGCL._SX3000_.jpg",
+        "https://m.media-amazon.com/images/I/81ypGyfK8mL._SX3000_.jpg",
+        "https://m.media-amazon.com/images/I/91Rd9TYGhNL._SX3000_.jpg",
+        "https://m.media-amazon.com/images/I/81qFlA2dqjL._SX3000_.jpg",
         "https://m.media-amazon.com/images/I/71DdKG7+jfL._SX3000_.jpg"
     ]
 
     return (
         <Carousel
-            IndicatorIcon=""
+            IndicatorIcon={false}
+            autoPlay={true}
+            animation="slide"
+            navButtonsAlwaysVisible={true}
+            cycleNavigation={true}
+            navButtonsProps={{
+                style:{
+                    backgroundColor:"transparent",
+                    color:"white",
+                    borderRadius:0,
+                    marginTop:-22,
+                    height:"104px",
+                }
+            }}
         >
             {
                 bannerImgList.map((img, i) => (
--- src/Layouts/Home/HomeMainComp.jsx
+++ src/Layouts/Home/HomeMainComp.jsx
@@ -1,12 +1,135 @@
 import { Banner } from './Banner';
 import './Home.css';
-import './Banner.css';
 
 export const HomeMainComp = () => {
+
+    const cardBodyList = [
+        { text: 'Professional tools', img: 'https://images-eu.ssl-images-amazon.com/images/G/31/img17/Biss/2018/QC/Tools1x._SY116_CB424026090_.jpg' },
+        { text: 'Measuring instruments', img: 'https://images-eu.ssl-images-amazon.com/images/G/31/img17/Biss/2021/BAU_GW_Graphics/PCQC/2._SY116_CB643952439_.jpg' },
+        { text: 'Cleaning supplies', img: 'https://images-eu.ssl-images-amazon.com/images/G/31/img17/Biss/2021/BAU_GW_Graphics/PCQC/1._SY116_CB643952439_.jpg' },
+        { text: 'Medical supplies', img: 'https://images-eu.ssl-images-amazon.com/images/G/31/img17/Biss/2018/QC/med1x._SY116_CB424026091_.jpg' },
+        { text: 'Flight tickets', img: 'https://images-eu.ssl-images-amazon.com/images/G/31/img16/AmazonPayTravels/November/GWPercolate/Flight_186x116._SY116_CB653435429_.jpg' },
+        { text: 'Bus tickets', img: 'https://images-eu.ssl-images-amazon.com/images/G/31/img16/AmazonPayTravels/November/GWPercolate/Bus_186x116._SY116_CB653435429_.jpg' },
+        { text: 'Train tickets', img: 'https://images-eu.ssl-images-amazon.com/images/G/31/img16/AmazonPayTravels/November/GWPercolate/Train_186x116._SY116_CB653435429_.jpg' },
+        { text: 'Essential travel products', img: 'https://images-eu.ssl-images-amazon.com/images/G/31/img16/AmazonPayTravels/November/GWPercolate/essential_186x116._SY116_CB653435429_.jpg' },
+        { text: 'Cleaning accessories', img: 'https://images-eu.ssl-images-amazon.com/images/G/31/img17/Auto/2020/GW/PCQC/Glasscare1X._SY116_CB410830553_.jpg' },
+        { text: 'Tyre & rim care', img: 'https://images-eu.ssl-images-amazon.com/images/G/31/img17/Auto/2020/GW/PCQC/Rim_tyrecare1x._SY116_CB410830552_.jpg' },
+        { text: 'Helmets', img: 'https://images-eu.ssl-images-amazon.com/images/G/31/img17/Auto/2020/GW/PCQC/Vega_helmet_186x116._SY116_CB405090404_.jpg' },
+        { text: 'Vacuum cleaner', img: 'https://images-eu.ssl-images-amazon.com/images/G/31/img17/Auto/2020/GW/PCQC/Vaccum1x._SY116_CB410830552_.jpg' },
+    ]
+
     return (
         <div className="home_section">
             <div className="banner_part">
                 <Banner />
+            </div>
+            <div className="row mx-0 justify-content-center">
+                <div className="col-sm-3 category_card card my-2">
+                    <div className="card-body">
+                        <h4 className="">Professional tools, testing &amp; more</h4>
+                        <div className="row my-3">
+                            {cardBodyList.map((data, i) => (
+                                (i < 4) && <div className="col-sm-6" key={i}>
+                                    <img className="w-100" src={data.img} alt="" />
+                                    <p style={{ fontSize: 'x-small' }}>{data.text}</p>
+                                </div>
+                            ))}
+                        </div>
+                        <a href="https://www.amazon.in/b?node=12414705031&pf_rd_r=X4VGXBDAT5WMBM2S0TJJ&pf_rd_p=47d02811-c3b6-4046-96fa-b28a4288a4a7&pd_rd_r=e4da1f29-b11a-4ed8-a666-e2a64a0c6b0a&pd_rd_w=kY1oA&pd_rd_wg=fBWlZ&ref_=pd_gw_unk">Explore All</a>
+                    </div>
+                </div>
+                <div className="col-sm-3 category_card card my-2">
+                    <div className="card-body">
+                        <h4 className="">Amazon Pay | Book your travel tickets</h4>
+                        <div className="row my-3">
+                            {cardBodyList.map((data, i) => (
+                                (i >= 4 && i <= 7) && <div className="col-sm-6" key={i}>
+                                    <img className="w-100" src={data.img} alt="" />
+                                    <p style={{ fontSize: 'x-small' }}>{data.text}</p>
+                                </div>
+                            ))}
+                        </div>
+                        <a href="https://www.amazon.in/gp/sva/dashboard?ref_=gw_apay_acko_qcpc&pf_rd_r=HBY2EQGFPMK9DF32T0N5&pf_rd_p=dec4af99-e62d-4c85-8275-9e13a6c47b32&pd_rd_r=ba8653dc-649a-4d2c-89f5-322119aacc44&pd_rd_w=INHX3&pd_rd_wg=qICUT">Explore more from amazon pay</a>
+                    </div>
+                </div>
+                <div className="col-sm-3 category_card card my-2">
+                    <div className="card-body">
+                        <h4 className="">Automotive essentials | Up to 60% off</h4>
+                        <div className="row my-3">
+                            {cardBodyList.map((data, i) => (
+                                (i > 7) && <div className="col-sm-6" key={i}>
+                                    <img className="w-100" src={data.img} alt="" />
+                                    <p style={{ fontSize: 'x-small' }}>{data.text}</p>
+                                </div>
+                            ))}
+                        </div>
+                        <a href="https://www.amazon.in/b?ie=UTF8&node=4772060031&pf_rd_r=HBY2EQGFPMK9DF32T0N5&pf_rd_p=9ff36f11-82d6-4600-a8fb-e52bb32e171c&pd_rd_r=ba8653dc-649a-4d2c-89f5-322119aacc44&pd_rd_w=VGvYN&pd_rd_wg=qICUT&ref_=pd_gw_unk">See more</a>
+                    </div>
+                </div>
+                <div className="col-sm-3 category_card card my-2">
+                    <div className="card-body">
+                        <h4 className="">Pay your credit card bills on Amazon</h4>
+                        <div className="card my-3">
+                            <div className="card-body p-0">
+                                <img className="w-100" src="https://images-eu.ssl-images-amazon.com/images/G/31/img19/AmazonPay/Boson/Sid/CCBP/DesktopGateway_CategoryCard_379x304_CCBP._SY304_CB413372299_.jpg" alt="" />
+                            </div>
+                        </div>
+                        <a href="#">Pay Now</a>
+                    </div>
+                </div>
+                <div className="col-sm-3 category_card card my-2">
+                    <div className="card-body">
+                        <h4 className="">Professional tools, testing &amp; more</h4>
+                        <div className="row my-3">
+                            {cardBodyList.map((data, i) => (
+                                (i < 4) && <div className="col-sm-6" key={i}>
+                                    <img className="w-100" src={data.img} alt="" />
+                                    <p style={{ fontSize: 'x-small' }}>{data.text}</p>
+                                </div>
+                            ))}
+                        </div>
+                        <a href="https://www.amazon.in/b?node=12414705031&pf_rd_r=X4VGXBDAT5WMBM2S0TJJ&pf_rd_p=47d02811-c3b6-4046-96fa-b28a4288a4a7&pd_rd_r=e4da1f29-b11a-4ed8-a666-e2a64a0c6b0a&pd_rd_w=kY1oA&pd_rd_wg=fBWlZ&ref_=pd_gw_unk">Explore All</a>
+                    </div>
+                </div>
+                <div className="col-sm-3 category_card card my-2">
+                    <div className="card-body">
+                        <h4 className="">Amazon Pay | Book your travel tickets</h4>
+                        <div className="row my-3">
+                            {cardBodyList.map((data, i) => (
+                                (i >= 4 && i <= 7) && <div className="col-sm-6" key={i}>
+                                    <img className="w-100" src={data.img} alt="" />
+                                    <p style={{ fontSize: 'x-small' }}>{data.text}</p>
+                                </div>
+                            ))}
+                        </div>
+                        <a href="https://www.amazon.in/gp/sva/dashboard?ref_=gw_apay_acko_qcpc&pf_rd_r=HBY2EQGFPMK9DF32T0N5&pf_rd_p=dec4af99-e62d-4c85-8275-9e13a6c47b32&pd_rd_r=ba8653dc-649a-4d2c-89f5-322119aacc44&pd_rd_w=INHX3&pd_rd_wg=qICUT">Explore more from amazon pay</a>
+                    </div>
+                </div>
+                <div className="col-sm-3 category_card card my-2">
+                    <div className="card-body">
+                        <h4 className="">Automotive essentials | Up to 60% off</h4>
+                        <div className="row my-3">
+                            {cardBodyList.map((data, i) => (
+                                (i > 7) && <div className="col-sm-6" key={i}>
+                                    <img className="w-100" src={data.img} alt="" />
+                                    <p style={{ fontSize: 'x-small' }}>{data.text}</p>
+                                </div>
+                            ))}
+                        </div>
+                        <a href="https://www.amazon.in/b?ie=UTF8&node=4772060031&pf_rd_r=HBY2EQGFPMK9DF32T0N5&pf_rd_p=9ff36f11-82d6-4600-a8fb-e52bb32e171c&pd_rd_r=ba8653dc-649a-4d2c-89f5-322119aacc44&pd_rd_w=VGvYN&pd_rd_wg=qICUT&ref_=pd_gw_unk">See more</a>
+                    </div>
+                </div>
+                <div className="col-sm-3 category_card card my-2">
+                    <div className="card-body">
+                        <h4 className="">Pay your credit card bills on Amazon</h4>
+                        <div className="card my-3">
+                            <div className="card-body p-0">
+                                <img className="w-100" src="https://images-eu.ssl-images-amazon.com/images/G/31/img19/AmazonPay/Boson/Sid/CCBP/DesktopGateway_CategoryCard_379x304_CCBP._SY304_CB413372299_.jpg" alt="" />
+                            </div>
+                        </div>
+                        <a href="#">Pay Now</a>
+                    </div>
+                </div>
             </div>
         </div>
     )
