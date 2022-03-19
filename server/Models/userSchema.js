cf42afe6a77566bbc7df2f096821b0acbe08ff0b
Disha Vaghela <dishav.inexture@gmail.com>
2022-03-19 20:07:10 +0530

    Signup data added to mongodb using POST method and validations fixes.

---
+++ server/Models/userSchema.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = mongoose.Schema({
+    fullname: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error("Not a valid email address");
+            }
+        }
+    },
+    mNumber: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    tokens: [
+        {
+            token: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+    carts: Array
+});
+
+const Users = mongoose.model("User", userSchema);
+
+module.exports = Users;
--- server/routes/router.js
+++ server/routes/router.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const Products = require("../Models/productSchema");
+const Users = require("../Models/userSchema");
+const bcrypt = require('bcryptjs');
 
 //To get all products data from db.
 router.get("/getproducts", async (req, res) => {
@@ -10,5 +12,36 @@ router.get("/getproducts", async (req, res) => {
         console.log("Error", error.message);
     }
 });
+
+//To register user for signup.
+router.post("/register", async (req, res) => {
+    // console.log(req.body);
+    const { fullname, email, mNumber, password } = req.body;
+
+    if (!fullname || !email || !mNumber || !password) {
+        res.send({ error: "Fill all the details" });
+        console.log("One of the input data is missing.");
+    } else {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPswd = await bcrypt.hash(password, salt);
+
+        try {
+            const isEmail = await Users.findOne({ email: email });
+            const isMobile = await Users.findOne({ mNumber: mNumber });
+            if (isEmail) {
+                res.status(322).json({ error: "Your provided Email " + email + " has already been used." });
+            } else if (isMobile) {
+                res.status(422).json({ error: "Account already exists with the mobile number " + mNumber });
+            } else {
+                const UserData = new Users({ fullname, email, mNumber, password: hashedPswd });
+                await UserData.save();
+                res.send(UserData);
+            }
+        } catch (error) {
+            console.log("Error:", error.message);
+            res.send({ error });
+        }
+    }
+})
 
 module.exports = router;
