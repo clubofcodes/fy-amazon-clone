76acf78f70ec9969d28dfe6d4c3ba5eddd6bdf88
Rahul Jagetiya <63360644+clubofcodes@users.noreply.github.com>
2022-04-08 10:08:04 +0530

    Added redux to user context.

--- client/src/API/AmazonNodeAPI.js
+++ client/src/API/AmazonNodeAPI.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: "http://localhost:3575/api",
+  baseURL: "https://amazon-api.vercel.app/api",
 });
 //baseURL: "http://localhost:3575/api",
 //baseURL: "https://amazon-api.vercel.app/api",
